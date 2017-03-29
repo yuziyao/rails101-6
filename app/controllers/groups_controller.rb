643b292f5612e57ad4e8da473fb18c7ea2347b0b
yoyo <yuziyao2007@163.com>
2017-03-29 10:29:58 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
-
+    current_user.join!(@group)
     redirect_to groups_path
   else
     render :new
@@ -62,7 +62,7 @@ end
 
 def quit
   @group = Group.find(params[:id])
-  
+
   if current_user.is_member_of?(@group)
     current_user.quit!(@group)
     flash[:alert] = "已退出本讨论版！"
