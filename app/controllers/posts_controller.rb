6db417b0e65a11e46dc4c22b471a6c7083aab10c
yoyo <yuziyao2007@163.com>
2017-03-31 08:35:34 +0800

    implement post update function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+
+
+  if @post.update(post_params)
+    redirect_to group_path(@group), notice: "Update Success!"
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+
+  @post.destroy
+  redirect_to group_path(@group), alert: "Post Deleted!"
+end
+
   private
 
   def post_params
